53308f6fad899b3111d5a6e349e19d3cb406b91a
Samuel Schlesinger <sgschlesinger@gmail.com>
2016-04-30 19:07:33 -0400

    Extended the Feature

---
+++ example/example.hs
@@ -0,0 +1,54 @@
+import System.Environment
+import System.IO
+import Feature
+
+features :: [Feature]
+
+main = do
+        args <- getArgs
+        if args == [] 
+            then run ""          [mainFeature] []
+            else run (head args) [mainFeature] (tail args)
+
+mainFeature = Program "fs" features "write | read | intersperse | copy | copy | usage"
+
+features = [
+            Command "write" writef "<filename> {<stuff-to-write>}",
+            Command "read" readf "<filename>",
+            Command "intersperse" interspersef "<filename> <spacer> {<stuff-to-write>}",
+            Command "copy" copyf "<filename> <newfilename>",
+            Command "cp" copyf "<filename> <newfilename>",
+            Command "usage" usagef ""
+           ] 
+
+-- | Prints the usage information
+usagef :: [String] -> IO ()
+usagef _ = putStr $ usage features
+
+-- | Copies a file
+copyf :: [String] -> IO ()
+copyf (filename:newfilename:_) = do { x <- readFile filename; writeFile newfilename x; }
+copyf _ = putStr "Missing arguments to copy: Usage cmd copy <filename> <newfilename>\n"
+
+-- | Writes each argument to the file supplied, space separated ;)
+interspersef :: [String] -> IO ()
+interspersef (filename:space:args) = withFile filename AppendMode (\h -> i' h space args) where
+                                        i' h _ [] = hClose h
+                                        i' h _ (x:[]) = do { hPutStr h x; hClose h; }
+                                        i' h space (x:xs) = do { hPutStr h (x ++ space); i' h space xs }
+interspersef _ = putStr "Missing arguments to intersperse: Usage cmd intersperse <filename> <spacer> {<stuff-to-write>}\n"
+
+-- | Reads a file and prints it to the screen
+readf :: [String] -> IO ()
+readf [] = putStr "Missing arguments to read: filename\nUsage: cmd read <filename>\n"
+readf (filename:_) = do
+                  contents <- readFile filename 
+                  putStr contents
+
+-- | Writes each argument to the file supplied, space separated
+writef :: [String] -> IO ()
+writef [] = putStr "Missing arguments to write: filename\nUsage: cmd write <filename> {<stuff>}\n"
+writef (filename:args) = withFile filename AppendMode (\h -> write' h args) where
+                           write' h [] = hClose h 
+                           write' h (x:[]) = do { hPutStr h x; hClose h; }
+                           write' h (x:xs) = do { hPutStr h (x ++ " "); write' h xs; }
